b5939c7ac511f3c8ccd34bccd6406dcbd4021695
Michał Ryz <michalryz2003@gmail.com>
2026-05-08 12:49:24 +0200

    feat: added authenticateWithGoogle function in auth service

--- globetrottr_backend/src/main/java/sniezynki/agh/globetrottr/user/AuthService.java
+++ globetrottr_backend/src/main/java/sniezynki/agh/globetrottr/user/AuthService.java
@@ -1,15 +1,25 @@
 package sniezynki.agh.globetrottr.user;
 
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.gson.GsonFactory;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import sniezynki.agh.globetrottr.security.JwtService;
 import sniezynki.agh.globetrottr.user.dto.AuthRequest;
 import sniezynki.agh.globetrottr.user.dto.AuthResponse;
+import sniezynki.agh.globetrottr.user.dto.GoogleAuthRequest;
 import sniezynki.agh.globetrottr.user.dto.RegisterRequest;
+
+import java.util.Collections;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -19,6 +29,9 @@ public class AuthService {
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
     private final AuthenticationManager authenticationManager;
+
+    @Value("${google.client.id}")
+    private String googleClientId;
 
     public AuthResponse register(RegisterRequest request) {
         var normalizedUsername = request.username().toLowerCase().trim();
@@ -60,6 +73,52 @@ public class AuthService {
                 .orElseThrow();
 
         var jwtToken = jwtService.generateToken(user);
+        return AuthResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    public AuthResponse authenticateWithGoogle(GoogleAuthRequest request) {
+        GoogleIdToken idToken;
+
+        try {
+            GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(new NetHttpTransport(), new GsonFactory())
+             // Comment  line below to test google login using https://developers.google.com/oauthplayground/
+             .setAudience(Collections.singletonList(googleClientId))
+                    .build();
+
+            idToken = verifier.verify(request.token());
+        } catch (Exception e) {
+            throw new BadCredentialsException("Failed to verify Google token");
+        }
+
+        if (idToken == null) {
+            throw new BadCredentialsException("Invalid Google ID token.");
+        }
+
+        GoogleIdToken.Payload payload = idToken.getPayload();
+        String email = payload.getEmail();
+        String name = (String) payload.get("name");
+
+        User user = userRepository.findByEmail(email).orElseGet(() -> {
+            User newUser = User.builder()
+                    .username(name.replaceAll("\\s+", "").toLowerCase() + UUID.randomUUID().toString().substring(0, 5))
+                    .email(email)
+                    .passwordHash(null)
+                    .authProvider(AuthProvider.GOOGLE)
+                    .isEmailVerified(true)
+                    .role(UserRole.USER)
+                    .build();
+            return userRepository.save(newUser);
+        });
+
+        if (!user.isEmailVerified()) {
+            user.setEmailVerified(true);
+            userRepository.save(user);
+        }
+
+        var jwtToken = jwtService.generateToken(user);
+
         return AuthResponse.builder()
                 .token(jwtToken)
                 .build();
