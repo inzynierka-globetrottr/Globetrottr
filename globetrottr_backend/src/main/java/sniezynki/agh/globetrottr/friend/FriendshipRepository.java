7bda00a31247627b89246a1f0d4a07bff3a82a32
majkelrice <michalryz2003@gmail.com>
2026-05-15 15:52:43 +0200

    feat: added blank FriendshipRepository

---
+++ globetrottr_backend/src/main/java/sniezynki/agh/globetrottr/friend/FriendshipRepository.java
@@ -0,0 +1,8 @@
+package sniezynki.agh.globetrottr.friend;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FriendshipRepository extends JpaRepository<Friendship, Long> {
+}
