d30fd84672e2c6d52b4c723e34bf2db457c06c76
Michał Ryz <michalryz2003@gmail.com>
2026-05-16 12:05:32 +0200

    feat: added new field in FriendshipResponse

--- globetrottr_backend/src/main/java/sniezynki/agh/globetrottr/friend/dto/FriendshipResponse.java
+++ globetrottr_backend/src/main/java/sniezynki/agh/globetrottr/friend/dto/FriendshipResponse.java
@@ -1,10 +1,10 @@
 package sniezynki.agh.globetrottr.friend.dto;
 
-import jakarta.validation.constraints.NotNull;
 import sniezynki.agh.globetrottr.friend.InviteStatus;
 
 public record FriendshipResponse(
-        @NotNull String username,
-        @NotNull InviteStatus  status
+        String username,
+        InviteStatus  status,
+        Boolean isIncomingRequest
         ) {
 }
