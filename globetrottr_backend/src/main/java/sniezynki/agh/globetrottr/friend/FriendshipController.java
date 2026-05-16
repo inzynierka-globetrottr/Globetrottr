b0e089157821116eedee42a5fe73e519575d4893
Michał Ryz <michalryz2003@gmail.com>
2026-05-16 12:06:28 +0200

    feat: defined endpoints in FriendshipController

--- globetrottr_backend/src/main/java/sniezynki/agh/globetrottr/friend/FriendshipController.java
+++ globetrottr_backend/src/main/java/sniezynki/agh/globetrottr/friend/FriendshipController.java
@@ -2,8 +2,10 @@ package sniezynki.agh.globetrottr.friend;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import sniezynki.agh.globetrottr.friend.dto.FriendshipRequest;
+import sniezynki.agh.globetrottr.friend.dto.FriendshipResponse;
 
 import java.util.List;
 
@@ -11,16 +13,70 @@ import java.util.List;
 @RequestMapping("/api/friends")
 @RequiredArgsConstructor
 public class FriendshipController {
-    private final FriendshipRepository friendshipRepository;
     private final FriendshipService friendshipService;
 
     @GetMapping
-    public ResponseEntity<List<Friendship>> getFriendships(@PathVariable String username) {
-        return ResponseEntity.ok(friendshipService.getUserFriends(username);
+    public ResponseEntity<List<FriendshipResponse>> getFriendships() {
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        return ResponseEntity.ok(friendshipService.getUserFriends(username));
     }
 
-    @PostMapping
-    public ResponseEntity<Friendship> sendInvite(@RequestBody FriendshipRequest request) {
-        return ResponseEntity.ok(friendshipService.sendInvite(request);
+    @GetMapping("/invites")
+    public ResponseEntity<List<FriendshipResponse>> getFriendRequests() {
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        return ResponseEntity.ok(friendshipService.getUserFriendRequests(username));
+    }
+
+    @GetMapping("/invites/sent")
+    public ResponseEntity<List<FriendshipResponse>> getSentFriendRequests() {
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        return ResponseEntity.ok(friendshipService.getSentFriendRequests(username));
+    }
+
+    @GetMapping("/{targetUsername}/status")
+    public ResponseEntity<FriendshipResponse> getFriendshipStatus(@PathVariable String targetUsername) {
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        return ResponseEntity.ok(friendshipService.getFriendshipStatus(username, targetUsername));
+    }
+
+    @PostMapping("/invites")
+    public ResponseEntity<Void> sendInvite(@RequestBody FriendshipRequest request) {
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        friendshipService.sendInvite(username, request.username());
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/invites/{targetUsername}/accept")
+    public ResponseEntity<Void> acceptInvite(@PathVariable String targetUsername) {
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        friendshipService.acceptInvite(username, targetUsername);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{targetUsername}")
+    public ResponseEntity<Void> deleteFriendOrRejectInvite(@PathVariable String targetUsername) {
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        friendshipService.deleteFriendOrRejectInvite(username, targetUsername);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/{targetUsername}/block")
+    public ResponseEntity<Void> blockUser(@PathVariable String targetUsername) {
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        friendshipService.blockUser(username, targetUsername);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{targetUsername}/block")
+    public ResponseEntity<Void> unblockUser(@PathVariable String targetUsername) {
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        friendshipService.unblockUser(username, targetUsername);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<FriendshipResponse>> searchUsers(@RequestParam String query) {
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        return ResponseEntity.ok(friendshipService.searchUsers(username, query));
     }
 }
