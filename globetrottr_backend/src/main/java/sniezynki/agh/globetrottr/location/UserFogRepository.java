483da1974189e05fd92ee166c8b40fe2a166278c
majkelrice <michalryz2003@gmail.com>
2026-05-18 16:12:07 +0200

    feat: added new native query to fetch users fog as geoJson

--- globetrottr_backend/src/main/java/sniezynki/agh/globetrottr/location/UserFogRepository.java
+++ globetrottr_backend/src/main/java/sniezynki/agh/globetrottr/location/UserFogRepository.java
@@ -1,9 +1,15 @@
 package sniezynki.agh.globetrottr.location;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import java.util.Optional;
 import java.util.UUID;
 
 public interface UserFogRepository extends JpaRepository<UserFog, Long> {
     Optional<UserFog> findByUser_UserId(UUID userId);
+
+    @Query(value = "SELECT ST_AsGeoJSON(fog_area) FROM user_fog uf JOIN users u ON uf.user_id = u.user_id WHERE u.username = :username", nativeQuery = true)
+    Optional<String> findFogGeoJsonByUsername(@Param("username") String username);
 }
