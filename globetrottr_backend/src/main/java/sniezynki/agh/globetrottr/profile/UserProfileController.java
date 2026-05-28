61e8d517e81a78a5d9caab9e21db91b931975b09
Michał Ryz <michalryz2003@gmail.com>
2026-05-28 14:22:21 +0200

    feat: added UserProfileController

---
+++ globetrottr_backend/src/main/java/sniezynki/agh/globetrottr/profile/UserProfileController.java
@@ -0,0 +1,47 @@
+package sniezynki.agh.globetrottr.profile;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import sniezynki.agh.globetrottr.profile.dto.UpdateBioRequest;
+import sniezynki.agh.globetrottr.profile.dto.UserProfileResponse;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/profile")
+@RequiredArgsConstructor
+public class UserProfileController {
+    private final UserProfileService userDetailsService;
+
+    @GetMapping("/me")
+    public ResponseEntity<UserProfileResponse> getMyProfile() {
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        return ResponseEntity.ok(userDetailsService.getMyProfile(username));
+    }
+
+    @GetMapping("/{friendUsername}")
+    public ResponseEntity<UserProfileResponse> getFriendProfile(@PathVariable String friendUsername) {
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        return ResponseEntity.ok(userDetailsService.getFriendProfile(username,friendUsername));
+    }
+
+    @PatchMapping("/bio")
+    public ResponseEntity<Void> updateBio(@Valid @RequestBody UpdateBioRequest request) {
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        userDetailsService.updateBio(username, request.bio());
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping(value = "/avatar", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<Map<String, String>> uploadAvatar(@RequestParam("file") MultipartFile file) {
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        String avatarUrl = userDetailsService.uploadAvatar(username, file);
+
+        return ResponseEntity.ok(Map.of("avatarUrl", avatarUrl));
+    }
+}
